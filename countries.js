a41c500df620ca64edd9263c7d684655111589d5
桂花糊 <ymp.sea@gmail.com>
2024-08-25 17:42:17 -0700

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,7 +1,8 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
-    ITA: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    CHN: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    BEL: {fillKey: 'visited'},
 }
