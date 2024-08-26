0b821730557382fea3c4a973399f18c6e1c3d07f
桂花糊 <ymp.sea@gmail.com>
2024-08-25 17:48:37 -0700

    Update cities.js

--- cities.js
+++ cities.js
@@ -1,10 +1,12 @@
 CITIES =
 [
-   {name: 'Seattle, WA', latitude: 47.60621, longitude: -122.33207, radius: 3, fillKey: 'city', date: ''},
+   {name: 'Seattle, WA', latitude: 47.6061, longitude: 122.3328, radius: 3, fillKey: 'city', date: ''},
+   {name: 'Vancouver, BC', latitude: 49.2827, longitude: 123.1207, radius: 3, fillKey: 'city', date: ''},
+   {name: 'Portland, OR', latitude: 45.5152, longitude: 122.6784, radius: 3, fillKey: 'city', date: ''},
    {name: 'Brussels, Belgium', latitude: 50.85045, longitude: 4.34878, radius: 3, fillKey: 'city', date: '2023-03'},
    {name: 'Amsterdam, Netherlands', latitude: 52.3676, longitude: 4.9041, radius: 3, fillKey: 'city', date: '2023-03'},
    
-   {name: 'BDL', latitude: 41.93742, longitude: 72.68202, radius: 3, fillKey: 'city', date: '2013-08'},
+   {name: 'BDL', latitude: 41.9389, longitude: 72.6860, radius: 3, fillKey: 'city', date: '2013-08'},
    // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
    {name: 'Wuchang, Hubei', latitude: 30.5420, longitude: 114.3160, radius: 3, fillKey: 'city', date: ''},
    {name: 'Hankow, Hubei', latitude: 30.5820, longitude: 114.2719, radius: 3, fillKey: 'city', date: ''},
